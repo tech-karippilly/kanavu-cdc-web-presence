f0f05f6fa5d7c47730ae853723d10a1df3db0fb6
tech-karippilly <vishnukarippilly@gmail.com>
2026-06-24 00:50:45 +0530

    feat: add Instagram, Facebook, YouTube, WhatsApp social links

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -16,4 +16,10 @@ export const SITE = {
   ],
   mapEmbed:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3929.176!2d76.2999!3d9.9816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sKochi%2C%20Kerala!5e0!3m2!1sen!2sin!4v1700000000000",
+  social: {
+    instagram: "https://www.instagram.com/kanavucdc",
+    facebook: "https://www.facebook.com/kanavucdc",
+    youtube: "https://www.youtube.com/@kanavucdc",
+    whatsapp: "https://wa.me/919876543210?text=Hi%20Kanavu%20CDC%2C%20I%27d%20like%20to%20know%20more%20about%20your%20services.",
+  },
 };
